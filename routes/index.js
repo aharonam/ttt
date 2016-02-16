6e599157f26739d1325514a803b416e30cc24879
Aharon Amram <aharonam@gmail.com>
2016-02-16 15:13:57 +0200

    fix text

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('Slack TTT Commands \n'
       +'/ttt (in/out) [project](text) [time](hh:mm) -> project & time are optional\n'
-      +'/ttt inout[project](text) [date] (dd-mm-yyyy) [in time](HH:MM) [out time](HH:MM) -> project & time are optional\n'
+      +'/ttt inout[project](text) [date] (dd-mm-yyyy) [in time](HH:MM) [out time](HH:MM) -> project is optional\n'
       +'/ttt project(text) -> will set default project\n'
       +'/ttt report [yyyy-mm,mm]-> will send month report to user slack chanel\n'
       +'/ttt validate -> will vaildate current month and send missing or uncompleted reports');
