bc7575c0ec299ab6188e1dd548919b21d84a4311
Aharon Amram <aharonam@gmail.com>
2016-02-16 15:12:08 +0200

    add inout command

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Slack TTT Commands \n'
-      +'/ttt [action](in/out) [project](text) [time](hh:mm) -> project & time are optional\n'
+      +'/ttt (in/out) [project](text) [time](hh:mm) -> project & time are optional\n'
+      +'/ttt inout[project](text) [date] (dd-mm-yyyy) [in time](HH:MM) [out time](HH:MM) -> project & time are optional\n'
       +'/ttt project(text) -> will set default project\n'
       +'/ttt report [yyyy-mm,mm]-> will send month report to user slack chanel\n'
       +'/ttt validate -> will vaildate current month and send missing or uncompleted reports');
